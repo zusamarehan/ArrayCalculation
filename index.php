05f5a6301fb83dce6815f176f3ac76d9520168d0
Rehan KU <zrehan286@gmail.com>
2023-05-20 17:11:54 +0530

    Problem solved

--- index.php
+++ index.php
@@ -14,3 +14,58 @@ $array = [
 
 // print the index which has the maximum sum between each 1s => 1
 // print the array with all the sum values => [2, 70, 0, 10, 66]
+
+
+$allSums = [];
+for ($m = 0; $m <= count($array) - 1; $m++) {
+    $found = false;
+    $sum = 0;
+    for ($i = 0; $i <= count($array[$m]) -1 ; $i++) {
+        if ($found) {
+            if ($array[$m][$i] === -1) {
+                $found = false;
+                break;
+            } else {
+                $sum += $array[$m][$i];
+            }
+        }
+
+        if ($array[$m][$i] === -1) {
+            $found = true;
+        }
+    }
+    $allSums[] = $sum;
+}
+
+print_r("The index with maximum sum is ". (array_keys($allSums, max($allSums))[0]));
+echo "\n";
+print_r($allSums);
+
+//max($allSums) => 70
+//array_keys
+//print_r(array_keys($allSums,))[0];
+
+
+//var_dump(array_key_exists($allSums[$i]));
+//
+
+//
+//$dummy = [1, -1, 1, -1, 1];
+//$found = false;
+//$sum = 0;
+//for ($i = 0; $i <= count($dummy) -1 ; $i++) {
+//
+//    if ($found) {
+//        if ($dummy[$i] === -1) {
+//            $found = false;
+//            break;
+//        } else {
+//            $sum += $dummy[$i];
+//        }
+//    }
+//
+//    if ($dummy[$i] === -1) {
+//        $found = true;
+//    }
+//}
+//print_r($sum);
